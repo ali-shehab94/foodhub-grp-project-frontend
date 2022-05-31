e64b28b558deec619309af2b2087685b084b28c4
ali-shehab94 <ali.shehab.ut@gmail.com>
2022-05-31 10:27:56 +0300

    Updated styling on restaurants page and created restaurants.js

--- javascript/restaurants.js
+++ javascript/restaurants.js
@@ -1,76 +1,31 @@
 //Declaring a variable to store the response from the backend
 let myResponse = '';
 
-//AXIOS
-$("#test").click(function(){
-
 axios({
- method: 'get',
- url: 'http://localhost/Food-Hub-Back-end/getRestaurants.php'
+ method: 'post',
+ url: 'http://localhost/Food-Hub-Back-end/ali-restaurants.php',
 })
 
 .then(function (response) {
-    console.log(response);
-    console.log("received..");
- if (response){
+    if (response){
     myResponse = response;
-    console.log(myResponse.data.length);
-    console.log('HEY');
- }else {
-     console.log('response error with status code = ' + response.status);
- }
- }
-)});
+    console.log(myResponse.data);
+    }else {
+        console.log('response error with status code = ' + response.status);
+    }
+})
 
+console.log(myResponse.data.length)
+var cols = [];
 
-function displayJson() {
-    var list = myResponse.data;
-    var cols = [];
-        
-    for (var i = 0; i < list.length; i++) {
-        for (var k in list[i]) {
-            if (cols.indexOf(k) === -1) {
-                    
-                // Push all keys to the array
-                cols.push(k);
-            }
+for (var i = 0; i < myResponse.data.length; i++) {
+    for (var k in myResponse.data[i]) {
+        if (cols.indexOf(k) === -1) {
+            // Push all keys to the array
+            cols.push(k);
         }
     }
-        
-    // Create a table element
-    var table = document.createElement("table");
-        
-    // Create table row tr element of a table
-    var tr = table.insertRow(-1);
-        
-    for (var i = 0; i < cols.length; i++) {
-            
-        // Create the table header th element
-        var theader = document.createElement("th");
-        theader.innerHTML = cols[i];
-            
-        // Append columnName to the table row
-        tr.appendChild(theader);
-    }
-        
-    // Adding the data to the table
-    for (var i = 0; i < list.length; i++) {
-            
-        // Create a new row
-        trow = table.insertRow(-1);
-        for (var j = 0; j < cols.length; j++) {
-            var cell = trow.insertCell(-1);
-                
-            // Inserting the cell at particular place
-            cell.innerHTML = list[i][cols[j]];
-        }
-    }
-        
-    // Add the newly created table containing json data
-    var el = document.getElementById("table");
-    el.innerHTML = "";
-    el.appendChild(table);
 }
 
-
-
+displayJson()
+console.log("cols" + cols)
