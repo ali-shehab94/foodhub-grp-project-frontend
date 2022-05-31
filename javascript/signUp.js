81999878bc4d3f608b83e45fab0d3889dfca1dcb
Hamze-A <99362310+Hamze-A@users.noreply.github.com>
2022-05-31 15:43:09 +0300

    Updated Home page

--- javascript/signUp.js
+++ javascript/signUp.js
@@ -1,3 +1,17 @@
+// Upload image
+const image_input = document.querySelector("#image_input");
+var uploaded_image = "";
+image_input.addEventListener("change", function(){
+    const reader = new FileReader();
+    reader.addEventListener("load", () => {
+        uploaded_image = reader.result;
+        document.querySelector("#display_image").style.backgroundImage = `url(${uploaded_image})`;
+    });
+    reader.readAsDataURL(this.files[0]);
+})  
+
+
+
 //get gender
 let gender = null;
 $("#sign-up-btn").click(function(){
