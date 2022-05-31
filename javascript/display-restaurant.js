b0ba3935b09226321e2e72e1cbd39c886e1a7c4e
ali-shehab94 <ali.shehab.ut@gmail.com>
2022-05-31 19:23:25 +0300

    Created empty edit profile page and linked it to the rest of the pages

--- javascript/display-restaurant.js
+++ javascript/display-restaurant.js
@@ -1,25 +1,48 @@
 let restaurantId = localStorage.getItem("restaurant_id");
 console.log("rest id --> " +restaurantId)
 
-let data = new FormData();
-data.append('restaurant_id', restaurantId);
 
-axios({
-    method: 'get',
-    url: 'http://localhost/Food-Hub-Back-end/getRestaurant.php',
-    data: data,
-   })
-   
-.then(function (response) {
-// If we got a response at all
-if (response.data.success==true){
-    handleResponse(response.data);
-}else {
-    console.log(response.data)
-    console.log('response error with status code = ' + response);
+function getRestaurant() {
+    let data = new FormData();
+    data.append("restaurant_id", "1");
+    axios({
+        method: 'get',
+        url: 'http://localhost/Food-Hub-Back-end/getRestaurant.php',
+        data: data,
+    })
+       
+    .then(function (response) {
+        console.log(response.data)
+    // If we got a response at all
+    if (response.data != null){
+        console.log(response);
+    }else {
+        console.log(response.data)
+        console.log('response error with status code = ' + response);
+    }
+    })
 }
-})
+getRestaurant();
 
+
+
+// function parseData(obj) {
+//     for (const data of obj) {
+//         console.log(data);
+//         let cuisine = data.cuisine;
+//         let rate = data.rating;
+//         let RestaurantName = data.name;
+//         let location = data.address
+//         console.log(id)
+//         console.log(RestaurantName);
+//         console.log(rate)
+//         console.log(cuisine)
+//         console.log(location)
+//         let element = document.getElementById("rest-card-container");
+//         element.innerHTML += 
+                
+//     }
+// }
 
 
 // function parseData(obj) {
